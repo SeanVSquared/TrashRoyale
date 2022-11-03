17a98bf79c3d2daa5c03d78934fc16da60b78b76
Derek Norman <derek.norman@colorado.edu>
2022-11-03 01:50:59 -0600

    fixed issue with linking css and works now

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(
 
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/Resources'));
+app.use(express.static(__dirname + '/resources'));
 
 app.use(
     bodyParser.urlencoded({
