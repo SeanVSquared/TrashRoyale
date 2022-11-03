b4ebbe22c97dee5b30ef5c0b9435e48632b3ee14
Sean Vestecka <seve8485@colorado.edu>
2022-11-02 21:53:04 -0600

    Added Database Schemas for Users, Daily Challenges, and Random Challenges

---
+++ init_data/alter.sql
@@ -0,0 +1,13 @@
+-- ALTER.SQL: alter the existing tables to define intertable relations
+
+-- change the users to daily challenges table to define foreign keys
+ALTER TABLE users_to_dailys
+ADD CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users (user_id);
+ALTER TABLE users_to_dailys
+ADD CONSTRAINT challenge_id FOREIGN KEY (challenge_id) REFERENCES dailychallenges (challenge_id);
+
+-- change the users to daily challenges table to define foreign keys
+ALTER TABLE users_to_randoms
+ADD CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users (user_id);
+ALTER TABLE users_to_randoms
+ADD CONSTRAINT challenge_id FOREIGN KEY (challenge_id) REFERENCES randchallenges (challenge_id);
---
+++ init_data/create.sql
@@ -0,0 +1,72 @@
+-- CREATE.SQL: initialize the tables in the database
+
+-- Drop our tables initially on each initialization
+-- DROP TABLE IF EXISTS users CASCADE;
+-- Definetly don't do this in production!
+
+-- create a new table users, with columns for email and password
+-- Add a userid serial, streak infromation, and daily challenge information
+-- Include a method to store the user's clash royale tag
+CREATE TABLE IF NOT EXISTS users(
+  user_id SERIAL PRIMARY KEY,
+  email VARCHAR(60),
+  username VARCHAR(60),
+  password CHAR(60) NOT NULL,
+  current_streak SMALLINT,
+  max_streak SMALLINT,
+  daily_challenges_completed SMALLINT,
+  random_challenges_completed SMALLINT,
+  clash_tag VARCHAR(12)
+);
+
+-- create a new table to store RANDOM challenges that have been given out
+-- store the average cost of the deck independantly for convenience
+-- Store the ID of the cards for the clash royale API calls
+CREATE TABLE IF NOT EXISTS randchallenges(
+  challenge_id SERIAL PRIMARY KEY,
+  challenge_name VARCHAR(30),
+  average_cost REAL,
+  card_id_1 SMALLINT,
+  card_id_2 SMALLINT,
+  card_id_3 SMALLINT,
+  card_id_4 SMALLINT,
+  card_id_5 SMALLINT,
+  card_id_6 SMALLINT,
+  card_id_7 SMALLINT,
+  card_id_8 SMALLINT
+);
+
+-- create a new table to store DAILY challenges that have been given out
+-- store the average cost of the deck independantly for convenience
+-- Store the ID of the cards for the clash royale API calls
+-- Store the date that the challenge was created 
+CREATE TABLE IF NOT EXISTS dailychallenges(
+  challenge_id SERIAL PRIMARY KEY,
+  challenge_name VARCHAR(30),
+  average_cost REAL,
+  card_id_1 SMALLINT,
+  card_id_2 SMALLINT,
+  card_id_3 SMALLINT,
+  card_id_4 SMALLINT,
+  card_id_5 SMALLINT,
+  card_id_6 SMALLINT,
+  card_id_7 SMALLINT,
+  card_id_8 SMALLINT,
+  challenge_date DATE
+);
+
+-- create a new table to convert from a user's serial ID to the challenge ID based on
+-- if they have seen the challenge or completed it (DAILY)
+CREATE TABLE IF NOT EXISTS users_to_dailys(
+  user_id SMALLINT,
+  challenge_id SMALLINT,
+  is_completed BOOLEAN
+)
+
+-- create a new table to convert from a user's serial ID to the challenge ID based on
+-- if they have seen the challenge or completed it (RANDOM)
+CREATE TABLE IF NOT EXISTS users_to_randoms(
+  user_id SMALLINT,
+  challenge_id SMALLINT,
+  is_completed BOOLEAN
+)
