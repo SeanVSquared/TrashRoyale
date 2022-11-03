71d8b8c0a9af5f9f348d201a70dbf02f6413d6b9
Sean Vestecka <seve8485@colorado.edu>
2022-11-03 11:26:33 -0600

    Added some comments to the database testing code, Added meeting milestone minuted, fixed database creation

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+// import the package express for API purposes
 const express = require('express');
 const app = express();
+
+// Use pg-promise to make connections to the database as previously
 const pgp = require('pg-promise')();
 const bodyParser = require('body-parser');
+
+// package to help set the session object. USE req.session!
 const session = require('express-session');
+
+// Package to hash user passwords (very important!)
 const bcrypt = require('bcrypt');
+
+// Package Axios: Make HTTP requests to an external server from our server
 const axios = require('axios');
 
+// database configuration
 const dbConfig = {
     host: 'db',
     port: 5432,
@@ -14,104 +24,120 @@ const dbConfig = {
     password: process.env.POSTGRES_PASSWORD,
 };
 
+// Use pg-promise and make a handy shorthand constant
 const db = pgp(dbConfig);
 
-// test your database
+// Database connection test
 db.connect()
-    .then(obj => {
-      console.log('Database connection successful'); // you can view this message in the docker compose logs
-      obj.done(); // success, release the connection;
-    })
-    .catch(error => {
-        console.log('ERROR:', error.message || error);
-    });
+  .then(obj => {
+    // If the connection is successful, print to the console
+    console.log('Database connection successful');
+    // use the done function to close the database connection
+    obj.done();
+  })
+  .catch(error => {
+    // If there are any errors, print them to the console
+    console.log('ERROR:', error.message || error);
+});
 
+// Set the view engine to EJS
 app.set('view engine', 'ejs');
 
+// Initializing Session variables
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        saveUninitialized: false,
-        resave: false,
-    })
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+  })
 );
 
+// Specify that we are using json to parse the body of a request
 app.use(bodyParser.json());
 
+//
 app.use(express.static(__dirname + '/Resources'));
 
+//
 app.use(
     bodyParser.urlencoded({
         extended: true,
     })
 );
 
+// GET Request for /home
 app.get('/home', async (req, res) => {
-    //console.log(req.session.user.api_key);
-    //console.log(req.session.user.tag);
-    const tag = '%23LJV98808';
-    const battlelog = await axios({
-        
-        url: `https://api.clashroyale.com/v1/players/${tag}/battlelog`,
-            method: 'get',
-            dataType:'json',
-            headers: {
-                "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzNDFiNzYyLWNhOTItNDY3Ni1iMGRlLTc5ZTJlY2I4ZmUzZCIsImlhdCI6MTY2NzAwNjcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3Il0sInR5cGUiOiJjbGllbnQifV19.0xjC5DPFXOCLhv1xJgTbdjv1fpVbMYK65Ci8ZvEWf61x8EZ1fe_9YRY-ymCwftlewSyQtJ9dxn-AylLlb_gJMg',
-            }
-        })
+  // Test cases to verify user session
+  //console.log(req.session.user.api_key);
+  //console.log(req.session.user.tag);
 
-        const clanRankings = await axios({
-        
-            url: `https://api.clashroyale.com/v1/locations/57000006/rankings/clans`,
-                method: 'get',
-                dataType:'json',
-                headers: {
-                    // Bearer apikey
-                    "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzNDFiNzYyLWNhOTItNDY3Ni1iMGRlLTc5ZTJlY2I4ZmUzZCIsImlhdCI6MTY2NzAwNjcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3Il0sInR5cGUiOiJjbGllbnQifV19.0xjC5DPFXOCLhv1xJgTbdjv1fpVbMYK65Ci8ZvEWf61x8EZ1fe_9YRY-ymCwftlewSyQtJ9dxn-AylLlb_gJMg',
-                    // `Bearer ${req.session.user.api_key} does not work
-                }
-            })
+  // Specift a tag for making the API calls
+  const tag = '%23LJV98808';
+  // This will need to be parsed from a request later on
 
-        
-            var worstClan = clanRankings.data.items[998];
-            var worstClanTag = worstClan.tag.replace('#', '%23');
-        
-        const worstClanInfo = await axios({
-        
-            url: `https://api.clashroyale.com/v1/clans/${worstClanTag}`,
-                method: 'get',
-                dataType:'json',
-                headers: {
-                    "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzNDFiNzYyLWNhOTItNDY3Ni1iMGRlLTc5ZTJlY2I4ZmUzZCIsImlhdCI6MTY2NzAwNjcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3Il0sInR5cGUiOiJjbGllbnQifV19.0xjC5DPFXOCLhv1xJgTbdjv1fpVbMYK65Ci8ZvEWf61x8EZ1fe_9YRY-ymCwftlewSyQtJ9dxn-AylLlb_gJMg',
-                }
-            })
-    
-    
-    axios({
-        
-        url: `https://api.clashroyale.com/v1/players/${tag}`,
-            method: 'get',
-            dataType:'json',
-            headers: {
-                "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzNDFiNzYyLWNhOTItNDY3Ni1iMGRlLTc5ZTJlY2I4ZmUzZCIsImlhdCI6MTY2NzAwNjcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3Il0sInR5cGUiOiJjbGllbnQifV19.0xjC5DPFXOCLhv1xJgTbdjv1fpVbMYK65Ci8ZvEWf61x8EZ1fe_9YRY-ymCwftlewSyQtJ9dxn-AylLlb_gJMg',
-            }
-        })
-        .then(results => {
-            //console.log(results); // the results will be displayed on the terminal if the docker containers are running
-            res.render('pages/home', {
-                results: results,
-                battlelog: battlelog,
-                clanRankings: clanRankings,
-                worstClanInfo: worstClanInfo,
-            });
-        })
-        .catch(error => {
-            // Handle errors
-            console.log(error);
-            res.render('pages/login', {
-                message: 'Uh Oh looks like your account was not found', 
-            })
-        })
+  // Get the battlelog of the specified user tag
+  const battlelog = await axios({
+    url: `https://api.clashroyale.com/v1/players/${tag}/battlelog`,
+    method: 'get',
+    dataType:'json',
+    // We should not just leave the API key in here, we can make it an environment variable
+    headers: {
+      "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzNDFiNzYyLWNhOTItNDY3Ni1iMGRlLTc5ZTJlY2I4ZmUzZCIsImlhdCI6MTY2NzAwNjcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3Il0sInR5cGUiOiJjbGllbnQifV19.0xjC5DPFXOCLhv1xJgTbdjv1fpVbMYK65Ci8ZvEWf61x8EZ1fe_9YRY-ymCwftlewSyQtJ9dxn-AylLlb_gJMg',
+    }
+  })
+
+  // Get the clan rankings
+  const clanRankings = await axios({
+    url: `https://api.clashroyale.com/v1/locations/57000006/rankings/clans`,
+    method: 'get',
+    dataType:'json',
+    headers: {
+      // Bearer apikey
+      "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzNDFiNzYyLWNhOTItNDY3Ni1iMGRlLTc5ZTJlY2I4ZmUzZCIsImlhdCI6MTY2NzAwNjcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3Il0sInR5cGUiOiJjbGllbnQifV19.0xjC5DPFXOCLhv1xJgTbdjv1fpVbMYK65Ci8ZvEWf61x8EZ1fe_9YRY-ymCwftlewSyQtJ9dxn-AylLlb_gJMg',
+      // `Bearer ${req.session.user.api_key} does not work
+    }
+  })
+
+  
+      var worstClan = clanRankings.data.items[998];
+      var worstClanTag = worstClan.tag.replace('#', '%23');
+  
+  const worstClanInfo = await axios({
+  
+      url: `https://api.clashroyale.com/v1/clans/${worstClanTag}`,
+          method: 'get',
+          dataType:'json',
+          headers: {
+              "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzNDFiNzYyLWNhOTItNDY3Ni1iMGRlLTc5ZTJlY2I4ZmUzZCIsImlhdCI6MTY2NzAwNjcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3Il0sInR5cGUiOiJjbGllbnQifV19.0xjC5DPFXOCLhv1xJgTbdjv1fpVbMYK65Ci8ZvEWf61x8EZ1fe_9YRY-ymCwftlewSyQtJ9dxn-AylLlb_gJMg',
+          }
+      })
+
+  
+  axios({
+      
+      url: `https://api.clashroyale.com/v1/players/${tag}`,
+          method: 'get',
+          dataType:'json',
+          headers: {
+              "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzNDFiNzYyLWNhOTItNDY3Ni1iMGRlLTc5ZTJlY2I4ZmUzZCIsImlhdCI6MTY2NzAwNjcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3Il0sInR5cGUiOiJjbGllbnQifV19.0xjC5DPFXOCLhv1xJgTbdjv1fpVbMYK65Ci8ZvEWf61x8EZ1fe_9YRY-ymCwftlewSyQtJ9dxn-AylLlb_gJMg',
+          }
+      })
+      .then(results => {
+          //console.log(results); // the results will be displayed on the terminal if the docker containers are running
+          res.render('pages/home', {
+              results: results,
+              battlelog: battlelog,
+              clanRankings: clanRankings,
+              worstClanInfo: worstClanInfo,
+          });
+      })
+      .catch(error => {
+          // Handle errors
+          console.log(error);
+          res.render('pages/login', {
+              message: 'Uh Oh looks like your account was not found', 
+          })
+      })
 
 });
 
--- init_data/create.sql
+++ init_data/create.sql
@@ -1,8 +1,16 @@
 -- CREATE.SQL: initialize the tables in the database
 
 -- Drop our tables initially on each initialization
--- DROP TABLE IF EXISTS users CASCADE;
+-- DROP TABLE IF EXSISTS users_to_dailys CASCADE; DROP TABLE IF EXSISTS users_to_randoms CASCADE; DROP TABLE IF EXSISTS dailychallenges CASCADE; DROP TABLE IF EXISTS users CASCADE; DROP TABLE IF EXISTS randchallenges CASCADE;
 -- Definetly don't do this in production!
+
+-- create a new table to convert from a user's serial ID to the challenge ID based on
+-- if they have seen the challenge or completed it (DAILY)
+CREATE TABLE IF NOT EXISTS users_to_dailys(
+  user_id SMALLINT,
+  challenge_id SMALLINT,
+  is_completed BOOLEAN
+);
 
 -- create a new table users, with columns for email and password
 -- Add a userid serial, streak infromation, and daily challenge information
@@ -54,14 +62,6 @@ CREATE TABLE IF NOT EXISTS dailychallenges(
   card_id_8 SMALLINT,
   challenge_date DATE
 );
-
--- create a new table to convert from a user's serial ID to the challenge ID based on
--- if they have seen the challenge or completed it (DAILY)
-CREATE TABLE IF NOT EXISTS users_to_dailys(
-  user_id SMALLINT,
-  challenge_id SMALLINT,
-  is_completed BOOLEAN
-)
 
 -- create a new table to convert from a user's serial ID to the challenge ID based on
 -- if they have seen the challenge or completed it (RANDOM)
