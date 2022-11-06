e495e4ae5dd25af48e00ea14aadde78a0b67f2ae
Derek Norman <derek.norman@colorado.edu>
2022-11-06 12:36:15 -0700

    completed simple navbar when not logged in and on home page

--- index.js
+++ index.js
@@ -102,77 +102,71 @@ app.get('/cards', (req, res) => {
 
 // GET Request for /home
 app.get('/home', async (req, res) => {
-  // Test cases to verify user session
-  //console.log(req.session.user.api_key);
-  //console.log(req.session.user.tag);
+    //console.log(req.session.user.api_key);
+    //console.log(req.session.user.tag);
+    const tag = '%23LJV98808';
+    const battlelog = await axios({
+        
+        url: `https://api.clashroyale.com/v1/players/${tag}/battlelog`,
+            method: 'get',
+            dataType:'json',
+            headers: {
+                "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImM2MTZiY2MwLWE4NWItNDYwMC04YWZjLTZmNTFjZGZjNGIzYSIsImlhdCI6MTY2Nzc2MTcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3IiwiNzEuMzMuMjIyLjQ0Il0sInR5cGUiOiJjbGllbnQifV19.4AQR_gJFAMXY5cOrlEULmb87q0wEbcZ2sYsfku25W2gGOh8hscH3B9cCQownIPkXZBvcNDyObVGPPhljIpoksw',
+            }
+        })
 
-  // Specift a tag for making the API calls
-  const tag = '%23LJV98808';
-  // This will need to be parsed from a request later on
+        const clanRankings = await axios({
+        
+            url: `https://api.clashroyale.com/v1/locations/57000006/rankings/clans`,
+                method: 'get',
+                dataType:'json',
+                headers: {
+                    // Bearer apikey
+                    "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImM2MTZiY2MwLWE4NWItNDYwMC04YWZjLTZmNTFjZGZjNGIzYSIsImlhdCI6MTY2Nzc2MTcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3IiwiNzEuMzMuMjIyLjQ0Il0sInR5cGUiOiJjbGllbnQifV19.4AQR_gJFAMXY5cOrlEULmb87q0wEbcZ2sYsfku25W2gGOh8hscH3B9cCQownIPkXZBvcNDyObVGPPhljIpoksw',
+                    // `Bearer ${req.session.user.api_key} does not work
+                }
+            })
 
-  // Get the battlelog of the specified user tag
-  const battlelog = await axios({
-    url: `https://api.clashroyale.com/v1/players/${tag}/battlelog`,
-    method: 'get',
-    dataType:'json',
-    // We should not just leave the API key in here, we can make it an environment variable
-    headers: {
-      "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzNDFiNzYyLWNhOTItNDY3Ni1iMGRlLTc5ZTJlY2I4ZmUzZCIsImlhdCI6MTY2NzAwNjcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3Il0sInR5cGUiOiJjbGllbnQifV19.0xjC5DPFXOCLhv1xJgTbdjv1fpVbMYK65Ci8ZvEWf61x8EZ1fe_9YRY-ymCwftlewSyQtJ9dxn-AylLlb_gJMg',
-    }
-  })
-
-  // Get the clan rankings
-  const clanRankings = await axios({
-    url: `https://api.clashroyale.com/v1/locations/57000006/rankings/clans`,
-    method: 'get',
-    dataType:'json',
-    headers: {
-      // Bearer apikey
-      "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzNDFiNzYyLWNhOTItNDY3Ni1iMGRlLTc5ZTJlY2I4ZmUzZCIsImlhdCI6MTY2NzAwNjcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3Il0sInR5cGUiOiJjbGllbnQifV19.0xjC5DPFXOCLhv1xJgTbdjv1fpVbMYK65Ci8ZvEWf61x8EZ1fe_9YRY-ymCwftlewSyQtJ9dxn-AylLlb_gJMg',
-      // `Bearer ${req.session.user.api_key} does not work
-    }
-  })
-
-
-  var worstClan = clanRankings.data.items[998];
-  var worstClanTag = worstClan.tag.replace('#', '%23');
-  
-  const worstClanInfo = await axios({
-  
-      url: `https://api.clashroyale.com/v1/clans/${worstClanTag}`,
-          method: 'get',
-          dataType:'json',
-          headers: {
-              "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzNDFiNzYyLWNhOTItNDY3Ni1iMGRlLTc5ZTJlY2I4ZmUzZCIsImlhdCI6MTY2NzAwNjcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3Il0sInR5cGUiOiJjbGllbnQifV19.0xjC5DPFXOCLhv1xJgTbdjv1fpVbMYK65Ci8ZvEWf61x8EZ1fe_9YRY-ymCwftlewSyQtJ9dxn-AylLlb_gJMg',
-          }
-      })
-
-  
-  axios({
-      
-      url: `https://api.clashroyale.com/v1/players/${tag}`,
-          method: 'get',
-          dataType:'json',
-          headers: {
-              "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYzNDFiNzYyLWNhOTItNDY3Ni1iMGRlLTc5ZTJlY2I4ZmUzZCIsImlhdCI6MTY2NzAwNjcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3Il0sInR5cGUiOiJjbGllbnQifV19.0xjC5DPFXOCLhv1xJgTbdjv1fpVbMYK65Ci8ZvEWf61x8EZ1fe_9YRY-ymCwftlewSyQtJ9dxn-AylLlb_gJMg',
-          }
-      })
-      .then(results => {
-          //console.log(results); // the results will be displayed on the terminal if the docker containers are running
-          res.render('pages/home', {
-              results: results,
-              battlelog: battlelog,
-              clanRankings: clanRankings,
-              worstClanInfo: worstClanInfo,
-          });
-      })
-      .catch(error => {
-          // Handle errors
-          console.log(error);
-          res.render('pages/login', {
-              message: 'Uh Oh looks like your account was not found', 
-          })
-      })
+        
+            var worstClan = clanRankings.data.items[998];
+            var worstClanTag = worstClan.tag.replace('#', '%23');
+        
+        const worstClanInfo = await axios({
+        
+            url: `https://api.clashroyale.com/v1/clans/${worstClanTag}`,
+                method: 'get',
+                dataType:'json',
+                headers: {
+                    "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImM2MTZiY2MwLWE4NWItNDYwMC04YWZjLTZmNTFjZGZjNGIzYSIsImlhdCI6MTY2Nzc2MTcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3IiwiNzEuMzMuMjIyLjQ0Il0sInR5cGUiOiJjbGllbnQifV19.4AQR_gJFAMXY5cOrlEULmb87q0wEbcZ2sYsfku25W2gGOh8hscH3B9cCQownIPkXZBvcNDyObVGPPhljIpoksw',
+                }
+            })
+    
+    
+    axios({
+        
+        url: `https://api.clashroyale.com/v1/players/${tag}`,
+            method: 'get',
+            dataType:'json',
+            headers: {
+                "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImM2MTZiY2MwLWE4NWItNDYwMC04YWZjLTZmNTFjZGZjNGIzYSIsImlhdCI6MTY2Nzc2MTcwNCwic3ViIjoiZGV2ZWxvcGVyLzE1YzYxMjhiLTc5NTUtYmI0Yi00ZTA4LWQ0OTU5ZDUyMzYyYiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3MS41Ni4yMTcuMTg3IiwiNzEuMzMuMjIyLjQ0Il0sInR5cGUiOiJjbGllbnQifV19.4AQR_gJFAMXY5cOrlEULmb87q0wEbcZ2sYsfku25W2gGOh8hscH3B9cCQownIPkXZBvcNDyObVGPPhljIpoksw',
+            }
+        })
+        .then(results => {
+            //console.log(results); // the results will be displayed on the terminal if the docker containers are running
+            res.render('pages/home', {
+                results: results,
+                battlelog: battlelog,
+                clanRankings: clanRankings,
+                worstClanInfo: worstClanInfo,
+            });
+        })
+        .catch(error => {
+            // Handle errors
+            console.log(error);
+            res.render('pages/login', {
+                message: 'Uh Oh looks like your account was not found', 
+            })
+        })
 
 });
 
