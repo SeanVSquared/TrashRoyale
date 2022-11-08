015f8dd8a48305624cf840b1c933bb464a3f230b
Sean Vestecka <seve8485@colorado.edu>
2022-11-07 17:44:04 -0700

    Updated database creation schema for merging

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ const axios = require('axios');
 
 // database configuration
 const dbConfig = {
-    host: 'db',
-    port: 5432,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
+  host: 'db',
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD
 };
 
 // Use pg-promise and make a handy shorthand constant
