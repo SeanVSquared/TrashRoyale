17f0bf52268f27462a8ad491159bec22ab222069
DanielHatakeyama <hatacoyama@gmail.com>
2022-11-09 14:55:21 -0700

    did a lot of stuff making the sql init_data folder good, added attributes.ejs for testing and corresponding get request, fixed multiple bugs in the init_data this is ready to be merged with main

--- index.js
+++ index.js
@@ -168,10 +168,11 @@ app.get('/random', (req, res) => {
   res.render('pages/random');
 });
 
-// Get Request to update and test card database (Eventually turn into a better form to update card data dynamically)
+// Get Request to update and test card database
+// TODO: turn into a better form to update card data dynamically
+// TODO: add functionality to dynamically add attributes to cards through UI
 app.get('/cards', (req, res) => {
-  // Render the Cards Test page
-  console.log("attempting to render /cards");
+  console.log("GET/cards");
 
   // Query to list cards
   const query = 'SELECT * FROM CARDS';
@@ -189,9 +190,15 @@ app.get('/cards', (req, res) => {
         message: error.message,
       });
     });
+});
 
-    
+// Get Request to view and test attribute database 
+app.get('/attributes', (req, res) => {
+  console.log("GET/attributes");
+  
+  // TODO: Query to get attributes
 
+  res.render('pages/attributes');
 });
 
 // GET Request for /home
