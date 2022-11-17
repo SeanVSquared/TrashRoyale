1d962bd959115764dd0f2bb54d5848f8c1d7bb33
Niko Johns <nicholasjohnsmail@gmail.com>
2022-11-17 11:27:55 -0700

    commiting to local

--- init_data/00-create.sql
+++ init_data/00-create.sql
@@ -4,6 +4,11 @@
 -- DROP TABLE IF EXSISTS users_to_dailys CASCADE; DROP TABLE IF EXSISTS users_to_randoms CASCADE; DROP TABLE IF EXSISTS dailychallenges CASCADE; DROP TABLE IF EXISTS users CASCADE; DROP TABLE IF EXISTS randchallenges CASCADE;
 -- Definetly don't do this in production!
 
+
+-- TODO: Deck Datatype
+-- Would help streamline dailychallenges and have option to store and create custom challenge pool
+
+
 -- create a new table to convert from a user's serial ID to the challenge ID based on
 -- if they have seen the challenge or completed it (DAILY)
 CREATE TABLE IF NOT EXISTS users_to_dailys(
@@ -32,8 +37,9 @@ CREATE TABLE IF NOT EXISTS users(
 -- Store the ID of the cards for the clash royale API calls
 CREATE TABLE IF NOT EXISTS randchallenges(
   challenge_id SERIAL PRIMARY KEY,
-  challenge_name VARCHAR(30),
+  challenge_name VARCHAR(60),
   average_cost REAL,
+  fourcycle REAL,
   card_id_1 SMALLINT,
   card_id_2 SMALLINT,
   card_id_3 SMALLINT,
@@ -48,10 +54,12 @@ CREATE TABLE IF NOT EXISTS randchallenges(
 -- store the average cost of the deck independantly for convenience
 -- Store the ID of the cards for the clash royale API calls
 -- Store the date that the challenge was created 
+
 CREATE TABLE IF NOT EXISTS dailychallenges(
   challenge_id SERIAL PRIMARY KEY,
-  challenge_name VARCHAR(30),
+  challenge_name VARCHAR(60),
   average_cost REAL,
+  fourcycle REAL,
   card_id_1 SMALLINT,
   card_id_2 SMALLINT,
   card_id_3 SMALLINT,
@@ -69,4 +77,33 @@ CREATE TABLE IF NOT EXISTS users_to_randoms(
   user_id SMALLINT,
   challenge_id SMALLINT,
   is_completed BOOLEAN
-)
+);
+
+-- create a new table cards to store cards and relevent information
+
+CREATE TABLE IF NOT EXISTS cards(
+  -- Data Fields from API
+  card_id SERIAL PRIMARY KEY,
+  card_name VARCHAR(60) UNIQUE NOT NULL,
+  max_level SMALLINT,
+  cost SMALLINT,
+  -- Calculated Data
+  daily_challenge_use_count SMALLINT,
+  usage SMALLINT,
+  winrate SMALLINT,
+  -- image
+  icon_url VARCHAR(360)
+);
+
+-- create a new table attributes, that holds and controls all custom data associated with cards
+CREATE TABLE IF NOT EXISTS attributes(
+  atribute_id SERIAL PRIMARY KEY,
+  attribute VARCHAR(120) UNIQUE NOT NULL
+);
+
+-- cards to attributes
+CREATE TABLE IF NOT EXISTS cards_to_attributes(
+  atribute_id SMALLINT,
+  card_id SMALLINT
+);
+
