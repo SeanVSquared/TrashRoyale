8b10dfe4ea7b4dd42743d5350ab282ad41169a4d
Sean Vestecka <seve8485@colorado.edu>
2022-11-30 21:07:49 -0700

    Final touches on verification algorithm and parity for menu, remove some redundant console logs

--- init_data/05-insert-random.sql
+++ init_data/05-insert-random.sql
@@ -1,3 +1,4 @@
 -- Create test data for random challenge
-INSERT INTO randchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8, dothash) values ('Test Challenge', 5.1, 11, 1, 2, 11, 31, 84, 91, 86, 102, 455);
+INSERT INTO randchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8, dothash, dothash2, dothash3) values ('Test Challenge', 5.1, 11, 1, 2, 11, 31, 84, 91, 86, 102, 3036, 14287737919546, 1812132);
+INSERT INTO randchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8, dothash, dothash2, dothash3) values ('Test Challenge 2', 5.1, 11, 1, 2, 4, 6, 15, 19, 91, 92, 544, 115346530, 1548912);
 -- Just a good deck
