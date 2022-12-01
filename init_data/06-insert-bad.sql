2bbbb83b473522f5d0358d685751e47b7c0abbd4
Tobey Switzer <tosw4346@colorado.edu>
2022-11-30 23:53:05 -0700

    added a few more bad challenges

--- init_data/06-insert-bad.sql
+++ init_data/06-insert-bad.sql
@@ -1,4 +1,16 @@
 -- Create test data for rbad challenges
 INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('Goblin Deck', 5.1, 11, 3, 20, 41, 42, 61, 89, 95, 97);
 INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('Electric Deck', 5.1, 11, 34, 53, 64, 75, 76, 98, 99, 84);
+INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('Buildings', 5.1, 11, 78, 79, 80, 81, 82, 83, 84, 86);
+INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('Spawner Deck', 5.1, 11, 97, 85, 87, 88, 89, 90, 83, 101);
 INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('X-Bow', 5.1, 11, 1, 2, 11, 31, 84, 86, 91, 102);
+INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('Explosion Deck', 5.1, 11, 32, 14, 59, 65, 82, 88, 7, 94);
+INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('Building Breakers', 5.1, 11, 39, 59, 57, 67, 22, 7, 52, 60);
+INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('Skeleton Deck', 5.1, 11, 11, 13, 59, 26, 87, 57, 73, 21);
+INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('Expensive Deck', 5.1, 11, 29, 10, 76, 56, 48, 30, 5, 98);
+INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('Cheapest Deck', 5.1, 11, 31, 11, 32, 75, 97, 107, 102, 3);
+INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('All Spells', 5.1, 11, 91, 92, 93, 94, 96, 103, 104, 105);
+
+
+
+
