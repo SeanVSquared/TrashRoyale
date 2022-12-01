70d75d3dd204262f432e7d65de86e8d0b80c4457
Tobey Switzer <tosw4346@colorado.edu>
2022-11-30 21:55:14 -0700

    check for bad deck win

--- init_data/06-insert-bad.sql
+++ init_data/06-insert-bad.sql
@@ -1,4 +1,5 @@
 -- Create test data for rbad challenges
 INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('Goblin Deck', 5.1, 11, 3, 20, 41, 42, 61, 89, 95, 97);
 INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('Electric Deck', 5.1, 11, 34, 53, 64, 75, 76, 98, 99, 84);
+INSERT INTO badchallenges (challenge_name, average_cost, fourcycle, card_id_1, card_id_2, card_id_3, card_id_4, card_id_5, card_id_6, card_id_7, card_id_8) values ('X-Bow', 5.1, 11, 1, 2, 11, 31, 84, 86, 91, 102);
 
